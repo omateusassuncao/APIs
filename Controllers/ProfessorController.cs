b3493c5893f8ce25deb740fc8f23bcc3a7834e1e
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-04-02 14:56:47 -0300

    Método Add Professor

--- Controllers/ProfessorController.cs
+++ Controllers/ProfessorController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProfessoresApi.Models;
 
 namespace ProfessoresApi.Controllers;
 
 [ApiController]
-[Route("[controller")]
-public class ProfessorController
+[Route("[controller]")]
+public class ProfessorController : ControllerBase
 {
+
+    private static List<Professor> professores = new List<Professor>();
+
+    [HttpPost]
+    public void AdicionaProfessor([FromBody] Professor professor)
+    {
+        professores.Add(professor);
+        Console.WriteLine(professor.Nome + " / " + professor.Disciplina);
+    }
 
 }
---
+++ Models/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessoresApi.Models;
+
+public class Professor
+{
+    [Required(ErrorMessage = "O nome do Professor é obrigatório")]
+    public string Nome { get; set; }
+    [Required(ErrorMessage = "O CPF do Professor é obrigatório")]
+    [Range(10000000000, 99999999999, ErrorMessage = "O CPF deve conter 11 caracteres. Não utilize pontos outraços, apenas números.Ex:12345678910")]
+    public ulong CPF { get; set; }
+
+    [Required(ErrorMessage = "A Disciplina do Professor é obrigatório")]
+    public string Disciplina { get; set; }
+}
