113b5d63569ae7f9705169e0b0192ca23f4ba68c
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-04-02 22:27:29 -0300

    Aplicando padrões RESTFUL de resposta

--- Controllers/ProfessorController.cs
+++ Controllers/ProfessorController.cs
@@ -12,15 +12,15 @@ public class ProfessorController : ControllerBase
     private static int Id = 0;
 
     [HttpPost]
-    public void AdicionaProfessor([FromBody] Professor professor)
+    public IActionResult AdicionaProfessor([FromBody] Professor professor)
     {
         professor.Id = Id++;
         professores.Add(professor);
-        Console.WriteLine(professor.Nome + " / " + professor.Disciplina);
+        return CreatedAtAction(nameof(RecuperaProfessorPorId),new {id = professor.Id }, professor);
     }
 
     [HttpGet]
-    public IEnumerable<Professor> RecuperaFilmes(
+    public IEnumerable<Professor> RecuperaProfessor(
         [FromQuery] int skip = 0,
         [FromQuery] int take = 10)
     {
@@ -28,9 +28,11 @@ public class ProfessorController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public Professor? RecuperaProfessorPorId(int id)
+    public IActionResult RecuperaProfessorPorId(int id)
     {
-        return professores.FirstOrDefault(professor => professor.Id == id);
+        var professor = professores.FirstOrDefault(professor => professor.Id == id);
+        if (professor == null) return NotFound();
+        return Ok();
     }
 
 }
