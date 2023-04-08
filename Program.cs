30885872c7fcc5cfee25657d6a784b5962389e54
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-04-08 14:48:42 -0300

    Base de classes configuradas

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<ProfessorContext>(opts =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers().AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
